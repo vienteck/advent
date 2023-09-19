d841eb3a62667485efa2d299b62a85c512ae4ff1
vienteck <vienteck@protonmail.com>
2023-09-19 13:01:10 -0700

    completed 1st part of day5

--- day5/main.go
+++ day5/main.go
@@ -16,20 +16,50 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	grid := [9][80]string{}
+	lastitem := [9]int{}
 	line := 1
 	row := 7
+	var instructions [][3]int
 	for scanner.Scan() {
 		if line < 9 {
-			prepdata(scanner.Text(), row, &grid)
+			prepdata(scanner.Text(), row, &grid, &lastitem)
 			row--
 		}
-		if line > 11 {
+		if line > 10 {
 
-			instructions := getinstructions(scanner.Text())
-			log.Print(instructions)
+			instructions = append(instructions, getinstructions(scanner.Text()))
 		}
 		line++
 	}
+	//we have our base grid and out instructions now
+	for i := 0; i < len(instructions); i++ {
+		processStep(instructions[i], &grid, &lastitem)
+	}
+	results := []string{}
+	for i := 0; i < 9; i++ {
+		results = append(results, string(grid[i][lastitem[i]-1]))
+	}
+	log.Print(results)
+}
+func processStep(instructions [3]int, grid *[9][80]string, lastemptycell *[9]int) {
+	//[0] amount to move from column  [1] to columns [2]
+	//remove cells fom grid and put into a temporary slice
+	movefrom := instructions[1] - 1
+	moveto := instructions[2] - 1
+	temp := make([]string, instructions[0])
+	copy(temp, (*grid)[movefrom][lastemptycell[movefrom]-instructions[0]:lastemptycell[movefrom]])
+	for i := lastemptycell[movefrom] - instructions[0]; i < lastemptycell[movefrom]; i++ {
+		(*grid)[movefrom][i] = ""
+	}
+	log.Print(temp)
+	//update lastemptycell array
+	lastemptycell[movefrom] = lastemptycell[movefrom] - instructions[0]
+	//move cells to requested column
+	for i := len(temp) - 1; i >= 0; i-- {
+		(*grid)[moveto][lastemptycell[moveto]] = temp[i]
+		lastemptycell[moveto]++
+	}
+
 }
 func getinstructions(row string) [3]int {
 
@@ -53,10 +83,15 @@ func getinstructions(row string) [3]int {
 	return instructions
 }
 
-func prepdata(data string, row int, grid *[9][80]string) {
+func prepdata(data string, row int, grid *[9][80]string, lastitem *[9]int) {
+
 	for i, column := 1, 0; i < len(data); i++ {
+
 		if data[i-1] == '[' {
 			(*grid)[column][row] = string(data[i])
+			if lastitem[column] == 0 {
+				lastitem[column] = row + 1
+			}
 		}
 		if i%4 == 0 {
 			column++
