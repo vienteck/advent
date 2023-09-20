7e258d3d9a92ea5f2ceac9e3a1e81a1dd1a90bc3
vienteck <vienteck@protonmail.com>
2023-09-20 11:55:52 -0700

    Finished part 1 of day6

--- day6/main.go
+++ day6/main.go
@@ -13,14 +13,32 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
-	datastream := scanner.Text()
-	datatracker := map[string]bool{}
-	for i := 0; i < len(datastream); i++ {
-		_, ok := datatracker[string(datastream[i])]
+	ds := scanner.Text()
+	dt := map[string]int{}
+	pt := 1
+	for i := 0; i < len(ds); i++ {
+		log.Printf("Current Pointer: %v", pt)
+		log.Printf("Current character: %v", string(ds[i]))
+		//prep data
+		if i < 3 {
+			dt[string(ds[i])] = 1
+			continue
+		}
+		_, ok := dt[string(ds[i])]
+		if len(dt) > 3 && !ok {
+			dt[string(ds[i])] = 1
+			log.Print(dt)
+			break
+			//this should meant that we have a unique 4 characters in a row
+		}
 		if ok {
 			//If value already exists in our map that means we have a duplicate and our window needs to move
+			dt = map[string]int{}
+			pt = i + 2
+		} else {
+			dt[string(ds[i])] = 1
 		}
 
 	}
-	log.Print(datastream)
+	log.Print(ds)
 }
