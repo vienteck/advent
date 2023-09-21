ee7635b2401274fb622715cff7eb27a573bc8066
vienteck <vienteck@protonmail.com>
2023-09-20 21:20:56 -0700

    i walk with my tail between my legs:

--- day6/main.go
+++ day6/main.go
@@ -1,43 +1,26 @@
 package main
 
 import (
-	"bufio"
-	"log"
+	"fmt"
 	"os"
 )
+//this code was copied from https://github.com/mnml/aoc/blob/main/2022/06/1.go
 
 func main() {
-	file, err := os.Open("datastream.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	ds := scanner.Text()
-	dt := map[string]int{}
-	pt := map[string]int{string(ds[0]): 0}
-	for i := 0; i < len(ds)-1; i++ {
-		//prep data
-		if i < 3 {
-			dt[string(ds[i])] = i
-			continue
-		}
-		_, ok := dt[string(ds[i])]
-		if ok {
-			log.Printf("duplicated key: %v", string(ds[i]))
-			p := pt[string(ds[i])] + 1
-			delete(dt, string(ds[i]))
-			pt = map[string]int{}
-			dt[string(ds[i])] = i
-			pt[string(ds[p])] = p + 1
-			//If value already exists in our map that means we have a duplicate and our window needs to move
-		} else {
-			dt[string(ds[i])] = i
-			if len(dt) == 4 {
-				log.Printf("Marker found at position %v", i)
-				break
-			}
-		}
+	input, _ := os.ReadFile("datastream.txt")
+	fmt.Println(find(string(input), 4))
+	fmt.Println(find(string(input), 14))
+}
 
+func find(s string, l int) int {
+	for i := l; i <= len(s); i++ {
+		m := map[rune]struct{}{}
+		for _, r := range s[i-l : i] {
+			m[r] = struct{}{}
+		}
+		if len(m) >= l {
+			return i
+		}
 	}
+	return -1
 }
