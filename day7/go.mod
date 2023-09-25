137e34193aa37061aa0c2014377afcc3d994a84a
vienteck <vienteck@protonmail.com>
2023-09-24 21:13:13 -0700

    Very stuck

---
+++ day7/go.mod
@@ -0,0 +1,5 @@
+module day7
+
+go 1.21.1
+
+require github.com/sashabaranov/go-openai v1.15.3 // indirect
---
+++ day7/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("data.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	var pwd []string
+	var curdir string
+	cdsize := 0
+	sizes := make(map[string]int)
+	sizes["/"] = 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		sep := strings.Split(line, " ")
+		if line == "" {
+			continue
+		}
+		//these are commands
+		if string(line[0]) == "$" {
+			if strings.Contains(line, "cd") {
+				if cdsize != 0 {
+					val, pass := sizes[strings.Join(pwd, "/")]
+					if pass {
+						fmt.Printf("Value already exists in map %v", val)
+					}
+					sizes[strings.Join(pwd, "/")] = cdsize
+					cdsize = 0
+				}
+
+				if strings.Contains(line, "..") {
+					pwd = pwd[:len(pwd)-1]
+				} else {
+					if sep[2] == "/" {
+						pwd = []string{}
+					} else {
+						pwd = append(pwd, sep[2])
+					}
+				}
+				fmt.Println(pwd)
+				curdir = sep[2]
+				log.Print(curdir)
+			}
+
+		} else {
+			if sep[0] != "dir" {
+				val, err := strconv.Atoi(sep[0])
+				if err != nil {
+					log.Fatal(err)
+				}
+				cdsize += val
+			}
+			fmt.Println("     " + line)
+		}
+
+	}
+	if cdsize != 0 {
+		sizes[strings.Join(pwd, "/")] = cdsize
+		cdsize = 0
+	}
+
+	tracker := make(map[string]int)
+	for key, val := range sizes {
+		d := strings.Split(key, "/")
+		// fmt.Printf("%v (%v)\n", key, val)
+		for i := 0; i < len(d); i++ {
+			tracker[strings.Join(d, "/")] += val
+		}
+	}
+	solution := 0
+	for key, val := range tracker {
+		fmt.Printf("%v %v\n", key, val)
+		if val < 100000 {
+			solution += val
+		}
+	}
+	fmt.Print(solution)
+}
